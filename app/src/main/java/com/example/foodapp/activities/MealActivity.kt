93c488e75abcc82fff3b50c98cd3d5dbfd6e8ab4
Oguz Evirgen <evirgenoguz@gmail.com>
2022-12-27 11:17:07 +0300

    Added Room Database

--- app/src/main/java/com/example/foodapp/activities/MealActivity.kt
+++ app/src/main/java/com/example/foodapp/activities/MealActivity.kt
@@ -60,7 +60,7 @@ class MealActivity : AppCompatActivity() {
                 binding.tvArea.text = "Area: ${meal.strArea}"
                 binding.tvInstructionsDetail.text = meal.strInstructions
 
-                youtubeLink = meal.strYoutube
+                youtubeLink = meal.strYoutube.toString()
             }
 
         })
